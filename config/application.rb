46d8e433fa5d850d7dc53a2aab566f29e43c6a67
3maigenkou <kaipurio848@gmail.com>
2022-03-23 11:14:21 +0900

    英訳ファイル作成

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module BooksApp
     #
     config.time_zone = "Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale = :en
 
   end
 end
