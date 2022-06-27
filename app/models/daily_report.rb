23aeee913f6749bf57d7ecf8d10f2bcdd3f6ca73
3maigenkou <kaipurio848@gmail.com>
2022-06-27 10:38:57 +0900

    modelの設定

--- app/models/daily_report.rb
+++ app/models/daily_report.rb
@@ -1,3 +1,6 @@
 class DailyReport < ApplicationRecord
   belongs_to :user
+  validates :comment, as: :commentable, dependent: :destroy
+  validates :title, presence: true
+  validates :content, presence: true
 end
