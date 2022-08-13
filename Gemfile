b88cf433a25e0ea177a20363e6866013d9ab0129
3maigenkou <kaipurio848@gmail.com>
2022-08-13 10:28:41 +0900

    rubocopを通しました

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ end
 
 group :development do
   gem 'dummy_text_jp'
+  gem 'erb_lint', require: false
   gem 'faker'
   gem 'i18n_generators'
   gem 'letter_opener_web'
@@ -46,7 +47,6 @@ group :development do
   gem 'rubocop-rails', require: false
   gem 'spring'
   gem 'web-console', '>= 4.1.0'
-  gem 'erb_lint', require: false
 end
 
 group :test do
--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  test "editable?" do
+  test 'editable?' do
     alice = users(:alice)
     bob = users(:bob)
     day_one = reports(:day_one)
@@ -12,10 +12,10 @@ class ReportTest < ActiveSupport::TestCase
     assert_not day_one.editable?(bob)
   end
 
-  test "created_on" do
+  test 'created_on' do
     day_one = reports(:day_one)
 
-    assert_not_equal Date.today, day_one.created_at
-    assert_equal Date.today, day_one.created_on
+    assert_not_equal Time.zone.today, day_one.created_at
+    assert_equal Time.zone.today, day_one.created_on
   end
 end
